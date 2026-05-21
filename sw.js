b861c5cf83f81ac1db16d2fb8f9286516ec85971
mga499 <218299663+mga499@users.noreply.github.com>
2026-05-21 21:45:07 +0200

    Update service worker version to v1.1.4

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // ================================================
 
 // Change ce numéro à chaque mise à jour de ton site !
-const VERSION = 'v1.1.3';
+const VERSION = 'v1.1.4';
 const CACHE_NAME = 'zyntracode-' + VERSION;
 
 // Fichiers à mettre en cache
