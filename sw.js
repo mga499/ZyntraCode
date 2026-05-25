b22718732fc475175b3c63d71817f3c5afcd9879
mga499 <218299663+mga499@users.noreply.github.com>
2026-05-25 08:48:32 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // ================================================
 
 // Change ce numéro à chaque mise à jour de ton site !
-const VERSION = 'v1.2.29';
+const VERSION = 'v1.2.30';
 const CACHE_NAME = 'zyntracode-' + VERSION;
 
 // Fichiers à mettre en cache
